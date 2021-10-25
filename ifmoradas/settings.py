a3b191050e6f066bd20259fd21b08a79028e0906
Anthony Cruz <anthony.cruz@alfatransportes.com.br>
2021-10-25 15:26:24 -0300

    Heroku

--- ifmoradas/settings.py
+++ ifmoradas/settings.py
@@ -14,8 +14,6 @@ import os
 from pathlib import Path
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
