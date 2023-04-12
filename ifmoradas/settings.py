bd4dfdc2e88f1221251e180d41028f31a3afc1dd
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-04-12 11:39:26 -0300

    Configurações alteradas para usuário padrão de postgres

--- ifmoradas/settings.py
+++ ifmoradas/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ifmoradas',
         'USER': 'postgres',
-        'PASSWORD': 'admin'
+        'PASSWORD': 'postgres'
     }
 }
 
